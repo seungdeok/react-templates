07a0b795bc5ddcb77d3024e6725ce31bd05be9ea
정승덕 <akk9132@naver.com>
2023-05-07 18:31:21 +0900

    feat: Add redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Router } from '@/routes/router';
-import { GlobalStyles } from '@/styles/GlotalStyles';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
+
+import { Router } from '@/routes/router';
+import { store } from '@/redux/store';
+import { GlobalStyles } from '@/styles/GlotalStyles';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyles />
-      <Router />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Router />
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
