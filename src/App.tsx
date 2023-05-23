a02d0046bcdf57a42a385d11ccf9ae19bae11572
정승덕 <akk9132@naver.com>
2023-05-23 21:35:20 +0900

    refactor: Add recoil library

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Provider } from 'react-redux';
+import { RecoilRoot } from 'recoil';
 
 import { Router } from '@/routes/router';
-import { store } from '@/redux/store';
 import { GlobalStyles } from '@/styles/GlotalStyles';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <Provider store={store}>
+    <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <GlobalStyles />
         <Router />
       </QueryClientProvider>
-    </Provider>
+    </RecoilRoot>
   );
 }
 
